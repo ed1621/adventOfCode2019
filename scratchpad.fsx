80409b4fb61b392af716bd1a96289e844bee7e6b
Eduardo Martinez <Eduardo.Martinez7@extendhealth.com>
2019-12-11 17:02:06 -0700

    start scratchpad work for day 2

--- scratchpad.fsx
+++ scratchpad.fsx
@@ -1,1 +1,11 @@
-﻿
+﻿let splitIntoGroups sequence chunkSize = 
+    sequence |> Seq.chunkBySize chunkSize
+
+let list = [1;9;10;3;2;3;11;0;99;30;40;50]
+
+let getOpCode (codeChunk: list<int>) = 
+    let validOpCodes = [ 1; 2; 99 ]
+    let code = codeChunk.Head
+    if not (List.contains code validOpCodes) then failwith "Unknown OpCode"
+    code    
+
